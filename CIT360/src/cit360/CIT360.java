20d5f45943cfa52da1c79bff304efa39e848eef5
pedrosa-andre <andre.matheus.pedrosa@gmail.com>
2019-05-18 01:34:47 -0300

    BasicCollections folder created Set (HashSet) example added

--- CIT360/src/cit360/CIT360.java
+++ CIT360/src/cit360/CIT360.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package cit360;
+import cit360.BasicCollections.*;
 
 /**
  *
- * @author INV
+ * @author Andre Matheus
  */
 public class CIT360 {
 
@@ -16,6 +17,15 @@ public class CIT360 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        AllStudents class101 = new AllStudents();
+        class101.addStudent("James");
+        class101.addStudent("John");
+        class101.addStudent("Peter");
+        class101.addStudent("James");
+        //You can notice that when the studend is already registered but is
+        //added to the the set it doesn't affect the set at all because sets
+        //doesn't hold two equal elements.
+        System.out.println("Class students are: \n"+class101.getAllStudents());
     }
     
 }
