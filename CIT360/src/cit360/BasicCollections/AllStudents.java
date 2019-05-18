1172bc31e58ffeaf67ab722ea6faf80275e9bc46
pedrosa-andre <andre.matheus.pedrosa@gmail.com>
2019-05-18 01:34:47 -0300

    BasicCollections folder created Set (HashSet) example added

---
+++ CIT360/src/cit360/BasicCollections/AllStudents.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cit360.BasicCollections;
+import java.util.*;
+
+/**
+ * This clas is just a simple set of all students of a class.
+ * I used a set collection because as a class can not have two or
+ * more of the same student, so this collection is useful to avoid
+ * dobled registers.
+ * 
+ * @author Andre Matheus
+ */
+public class AllStudents {
+    
+    //private HashSet[] allStudents;
+    Set<String> allStudents = new HashSet<String>();
+
+    public AllStudents() {
+    }
+    
+    /**
+    * addStudent method
+    * Purpose: add students to the set pointing out the repeated ones
+    * Parameters: String with student's name
+    * Returns: none
+    */
+    public void addStudent(String student){
+        //this point out when a student is already on the set
+        for (String s : this.getAllStudents()){
+            if (s == student){
+                System.out.println( student + " is already registered");
+                break; 
+            };
+        }
+        allStudents.add(student);
+        //You can notice that when the studend is already registered but is
+        //added to the the set it doesn't affect the set at all because sets
+        //doesn't hold two equal elements.
+        System.out.println(student + " added!");
+    };
+
+    public Set<String> getAllStudents() {
+        return allStudents;
+    }
+
+    public void setAllStudents(Set<String> allStudents) {
+        this.allStudents = allStudents;
+    }
+    
+    
+
+}
