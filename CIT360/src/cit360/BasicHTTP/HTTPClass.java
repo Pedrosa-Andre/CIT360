f3eea111ae024d0c540a934ef95ccb4b68582fc2
pedrosa-andre <andre.matheus.pedrosa@gmail.com>
2019-06-12 08:28:32 -0300

    HTTPClass started

---
+++ CIT360/src/cit360/BasicHTTP/HTTPClass.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cit360.BasicHTTP;
+
+/**
+ *
+ * @author Andre Matheus
+ */
+public class HTTPClass {
+    
+    /**
+     * HTTP stands for Hypertext Transfer Protocol. It serves to define a way 
+     * messages will be formatted and was created to facilitate the use of 
+     * hypertext (text displayed in computer with hyperlinks [references to 
+     * other texts the reader can access]) in the World Wide Web. Comparatively, 
+     * it's like if you have a lot of texts and media in the web and wants to 
+     * create an address to each one of them to find and navigate through them
+     * more easily, so the HTTP would the standard to write the addresses.
+     * 
+     * The HTTP works as a request-response protocol. It involves two elements 
+     * (usually a client and a server) where the client asks the server for a 
+     * file using an HTTP with the file address and the server reads the HTTP, 
+     * verifies where is the file is located, and send it to the client. The
+     * HTTP can be also used to request actions, in this case, the server will 
+     * return a completion message after reading the HTTP and doing the request.
+     */
+    
+    
+}
