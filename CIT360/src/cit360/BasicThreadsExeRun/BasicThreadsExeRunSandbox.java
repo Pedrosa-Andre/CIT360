8462a6915abe083cb3f7e6fc8471fc9d0a1d70ed
pedrosa-andre <andre.matheus.pedrosa@gmail.com>
2019-06-26 08:23:14 -0300

    Started ThreadsExeRun class

---
+++ CIT360/src/cit360/BasicThreadsExeRun/BasicThreadsExeRunSandbox.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cit360.BasicThreadsExeRun;
+
+/**
+ *
+ * @author Andre Matheus
+ */
+public class BasicThreadsExeRunSandbox {
+    
+    // Okay, so let's start with this absolutely interesting subject that is 
+    // Threads, Executors, Runnables, and etc. I'm loving to learn about it.
+    
+    //                             -----Treads-----
+    // Threads are like occupied paths in the CPU memory where tasks can be 
+    // performed (I know there are beter definitions on the web, but I tried
+    // to use my words here). A process (an instance of a program in execution)
+    // can have one or multiple threads.
+    
+    //
+
+}
--- CIT360/src/cit360/CIT360.java
+++ CIT360/src/cit360/CIT360.java
@@ -9,7 +9,6 @@ import cit360.BasicJSON.BasicJSONSandbox;
 import cit360.BasicMVC.BasicMVCSandbox;
 import cit360.BasicHTTP.BasicHTTPSandbox;
 import cit360.BasicJUnit.StudentForm;
-import java.lang.Object;
 //import java.io.*;
 //import java.util.*;
 //import com.google.gson.*;
@@ -33,24 +32,7 @@ public class CIT360 {
         
         //To see basic HTTP sandbox examples uncomment the coment bellow
         //BasicHTTPSandbox.sandbox();
-        
-        StudentForm a1 = new StudentForm();
-        StudentForm a2 = new StudentForm();
-        Integer at = a1.getAge();
-        Integer bt = a2.getAge();
-        String a = "111";
-        String b = "111";
-        System.out.println(a1 == a2);
-        System.out.println(a1.equals(a2));
-        System.out.println(at == bt);
-        System.out.println(at.equals(bt));
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
-//        System.out.println(a1.getAge());
-        
-        
-        
-        
+                
     }
     
 }
